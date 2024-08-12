bb11e0256fb5877f5246912711a74a0860d62ab8
Jasson__Cu <jassoncucaal14@hotmail.com>
2024-08-12 13:14:59 -0600

    feat: :sparkles: Add `Currency` model

--- bank/models.py
+++ bank/models.py
@@ -1,3 +1,13 @@
 from django.db import models
+from django.utils.translation import gettext_lazy as _
 
 # Create your models here.
+
+
+class Currency(models.Model):
+    code = models.CharField(_('Código'), max_length=3, unique=True)
+    name = models.CharField(_('Nombre'), max_length=100, unique=True)
+    symbol = models.CharField(_('Símbolo'), max_length=1, unique=True)
+
+    def __str__(self):
+        return self.name
